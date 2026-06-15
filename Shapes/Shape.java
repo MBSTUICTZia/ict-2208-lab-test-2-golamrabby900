fa26a1687eb3af6ada43455fc2644ea943535716
golamrabby900 <golamrabby0908@gmail.com>
2026-06-15 11:01:42 +0600

    Update Shape.java

--- Shapes/Shape.java
+++ Shapes/Shape.java
@@ -1,24 +1,32 @@
 package shapes;
 
-public abstract class Shape {
-    private String  color;
+public abstract class Shape{
+    private String color;
     private boolean filled;
 
-    public Shape(String color, boolean filled) {
-        this.color  = color;
+    public Shape(String color, boolean filled){
+        this.color = color;
         this.filled = filled;
     }
 
     public abstract double getArea();
     public abstract double getPerimeter();
 
-    @Override
-    public String toString() {
-        return "Color : " + color + "\n" + "Filled: " + (filled ? "Yes" : "No");
+    @Override 
+    public String toString(){
+        return "Color : " + color + "\n" + "Filled: " + (filled? "Yes" : "No");
     }
 
-    public String  getColor()           { return color;  }
-    public boolean isFilled()           { return filled; }
-    public void    setColor(String c)   { this.color  = c; }
-    public void    setFilled(boolean f) { this.filled = f; }
+    public String getColor(){
+        return color;
+    }
+    public boolean isFilled(){
+        return filled;
+    }
+    public void setcolor(String color){
+        this.color = color;
+    }
+    public void setFilled (boolean filled){
+        this.filled = filled;
+    }
 }
